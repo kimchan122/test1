244469dcd14e6e10cb744551f63ffb77d3101fa7
kimchan122 <chanchankim122@gmail.com>
2020-10-11 20:16:47 +0900

    why

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Test1" << '\n';
+	cout << "Test2" << '\n';
+	cout << "Test3" << '\n';
+	cout << "Test4" << '\n';
+	cout << "해결좀하자" << '\n';
+	cout << "why korean an dem?" << '\n';
+	cout << "why korean an dem?" << '\n';
+	cout << "why korean an dem?" << '\n';
+	cout << "why korean an dem?" << '\n';
+	cout << "why korean an dem?" << '\n';
+	return 0;
+}
