82b1d5512959872080905056b1e44cd58a45c9d8
kimchan122 <kimchan122@naver.com>
2020-10-11 20:06:41 +0900

    why_x_5

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -8,5 +8,9 @@ int main()
 	cout << "Test4" << '\n';
 	cout << "해결좀하자" << '\n';
 	cout << "why korean an dem?" << '\n';
+	cout << "why korean an dem?" << '\n';
+	cout << "why korean an dem?" << '\n';
+	cout << "why korean an dem?" << '\n';
+	cout << "why korean an dem?" << '\n';
 	return 0;
 }
